1451f098d17d110d42545865b7be8ff3a0a77021
GliderV5 <60002998+GliderV5@users.noreply.github.com>
2023-09-07 11:07:06 +0200

    Update change-ubuntu-login-screen.py

--- change-ubuntu-login-screen.py
+++ change-ubuntu-login-screen.py
@@ -2,14 +2,15 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
+########## Fonctions ##########
+
 def file_location(titre):
     root = tk.Tk()
     root.withdraw()
-
     file_path = filedialog.askopenfilename(title=str(titre), filetypes=(("Fichiers JPG", "*.jpg"), ("Tous les fichiers", "*.*")))
-
     return str(file_path)
 
+########## Main ##########
 
 os.system("clear")
 print("choisissez une option : ")
@@ -18,7 +19,7 @@ print("(2) Restaurer l'arrière plan")
 print("(0) Quitter")
 choix = int(input(">>> "))
 
-if str(choix) == "1":
+if str(choix) == "1": # Appliquer arrière plan
     os.system("sudo apt install libglib2.0-dev-bin")
     os.system("clear")
     input("Veuillez maintenant choisir l'arrière plan à utiliser, Appyuez sur entrer pour continuer ...")
@@ -27,7 +28,7 @@ if str(choix) == "1":
     os.system("wget -qO - https://github.com/PRATAP-KUMAR/ubuntu-gdm-set-background/archive/main.tar.gz | tar zx --strip-components=1 ubuntu-gdm-set-background-main/ubuntu-gdm-set-background")
     os.system("sudo ./ubuntu-gdm-set-background --image "+location)
 
-elif str(choix) == "2":
+elif str(choix) == "2": # Restaurer les changements 
     os.system("clear")
     os.system("sudo ./ubuntu-gdm-set-background --reset")
 
